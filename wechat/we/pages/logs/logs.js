8b1e5d6bde3a49189532682e017c9f40ff73d7c5
rose07 <1223610629@qq.com>
2018-08-28 16:45:03 +0800

    选题添加

--- wechat/we/pages/logs/logs.js
+++ wechat/we/pages/logs/logs.js
@@ -3,19 +3,93 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    titleIndex: 1,
+    logs: [
+      {
+        name: '题目一', data: [{
+          name: 'aaaa',
+          open: false
+        },
+        {
+          name: 'aaaa',
+          open: true
+        },
+        {
+          name: 'bbbbb',
+          open: false
+        },
+        {
+          name: 'ccccccccccccc',
+          open: false
+        }, {
+          name: 'ddddddddddddddddd',
+          open: false
+        }]
+      }
+
+    ]
   },
   onLoad: function () {
+    
     this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+      // logs: (wx.getStorageSync('logs') || []).map(log => {
+      //   return util.formatTime(new Date(log))
+      // })
     })
   },
   //事件处理函数
   bindViewTap: function () {
-    wx.navigateTo({
-      url: '../home/home'
+    this.data.logs.map((it) => {
+      it.data.map((item) => {
+        if (item.open) {
+          this.next()
+        }
+      })
+    })
+
+  },
+  next:function(){
+    this.setData({
+      titleIndex: this.data.titleIndex++,
+      logs: [
+        {
+          name: '题目二', data: [{
+            name: 'aaaa',
+            open: false
+          },
+          {
+            name: 'aaaa',
+            open: false
+          },
+          {
+            name: 'bbbbb',
+            open: false
+          },
+          {
+            name: 'ccccccccccccc',
+            open: false
+          }, {
+            name: 'ddddddddddddddddd',
+            open: false
+          }]
+        }
+
+      ]
     })
   },
+  buttonNext: function (e) {
+    var it = e.target.dataset.it;
+    it = it.split('-');
+    var logs = this.data.logs;
+    logs.map((it) => {
+      it.data.map((item) => {
+        item.open = false;
+      })
+    })
+    logs[it[0]].data[it[1]].open = true;
+
+    this.setData({
+      logs: logs
+    })
+  }
 })
